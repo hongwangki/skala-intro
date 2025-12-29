1d984a65f19f75ab02dd8cb3e5363fccfb0aa9d1
홍왕기 <leejd8130@naver.com>
2025-12-29 16:33:41 +0900

    에코프로그램 개선

--- echo.py
+++ echo.py
@@ -1,3 +1,10 @@
-# Python 3: 사용자가 입력한 문장을 그대로 출력
-s = input("문장을 입력하시오: ")
-print("입력하신 문장은: "+ s)
+# Python 3: 문장을 반복 입력하고 그대로 출력 (!quit 입력 시 종료)
+
+while True:
+    s = input("문장을 입력하시오 (!quit 입력 시 종료): ")
+
+    if s == "!quit":
+        print("프로그램을 종료합니다.")
+        break
+
+    print("입력하신 문장은:", s)
